b5d92a0e0be938cd117c27691c8ffb3dc4b30d0b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-02 08:18:20 +0600

    refactor: Changed events that triggers an foldstyle ID update

--- plugin/foldtext.lua
+++ plugin/foldtext.lua
@@ -1,23 +1,35 @@
+---@type integer An autocmd group is used to group multiple event listeners together.
 local augroup = vim.api.nvim_create_augroup("foldtext", {});
 
-vim.api.nvim_create_autocmd("BufNew", {
+-- Update style for buffers whose option has changed.
+vim.api.nvim_create_autocmd("OptionSet", {
+	group = augroup,
+	callback = function (event)
+		---@type string[]
+		local valid = { "filetype", "buftype", "foldmethod", "foldexpr" };
+
+		if vim.list_contains(valid, event.match) then
+			for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+				require("foldtext").attach(buffer);
+			end
+		end
+	end
+});
+
+-- Update style for buffers that will be shown.
+vim.api.nvim_create_autocmd("BufWinEnter", {
 	group = augroup,
 	callback = function (event)
 		require("foldtext").attach(event.buf);
 	end
 });
 
--- Create autocmd
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function (event)
-		require("foldtext").attach(event.buf);
-	end
-})
-
+-- Clear the highlight group for fold text.
 vim.api.nvim_create_autocmd({
 	"VimEnter",
 	"ColorScheme"
 }, {
+	group = augroup,
 	callback = function ()
 		vim.api.nvim_set_hl(0, "Folded", {
 			link = "Normal"

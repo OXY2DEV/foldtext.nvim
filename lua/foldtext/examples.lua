2f808af2b73a8244e94a1b6683c4fbdc6a1a319b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-06 09:00:32 +0600

    chore: Updated examples

--- lua/foldtext/examples.lua
+++ lua/foldtext/examples.lua
@@ -1,25 +1,65 @@
+---@type table<string, foldtext.custom_style>
 local examples = {};
 
----@type foldtext.section
+--- A horizontal rule with the fold size.
 examples.fold_hr = {
-	kind = "section",
-	output = function (_, window)
-		local width = vim.api.nvim_win_get_width(window);
-		local textoff = vim.fn.getwininfo(window)[1].textoff;
+	condition = function ()
+		return true;
+	end,
+	parts = {
+		{
+			kind = "section",
+			output = function (_, window)
+				local width = vim.api.nvim_win_get_width(window);
+				local textoff = vim.fn.getwininfo(window)[1].textoff;
 
-		width = width - textoff;
+				width = width - textoff;
 
-		local size = (vim.v.foldend - vim.v.foldstart) + 1;
-		local len = vim.fn.strdisplaywidth(" " .. tostring(size) .. " lines ");
+				local size = (vim.v.foldend - vim.v.foldstart) + 1;
+				local len = vim.fn.strdisplaywidth(" " .. tostring(size) .. " lines ");
 
-		return {
-			{ string.rep("-", math.ceil((width - len) / 2)), "@comment" },
-			{ " " },
-			{ tostring(width), "@number" },
-			{ " lines ", "@comment" },
-			{ string.rep("-", math.floor((width - len) / 2)), "@comment" },
-		};
-	end
+				return {
+					{ string.rep("-", math.ceil((width - len) / 2)), "@comment" },
+					{ " " },
+					{ tostring(size), "@number" },
+					{ " lines ", "@comment" },
+					{ string.rep("-", math.floor((width - len) / 2)), "@comment" },
+				};
+			end
+		}
+	}
+};
+
+--- Markdown detail tags.
+examples.detail = {
+	condition = function (buffer, window)
+		return vim.bo[buffer].ft == "markdown" and vim.wo[window].foldmethod == "expr" and vim.wo[window].foldexpr == "v:lua.vim.treesitter.foldexpr()";
+	end,
+
+	parts = {
+		{
+			condition = function (buffer)
+				local next_line = table.concat(
+					vim.fn.getbufline(buffer, vim.v.lnum + 1)
+				);
+
+				return string.match(next_line, "%s*<summary>(.-)</summary>") ~= nil;
+			end,
+
+			kind = "section",
+			output = function (buffer)
+				local next_line = table.concat(
+					vim.fn.getbufline(buffer, vim.v.lnum + 1),
+					""
+				);
+				local summary = string.match(next_line, "%s*<summary>(.-)</summary>");
+
+				return {
+					{ summary or "hi", "@comment" }
+				};
+			end
+		}
+	}
 };
 
 return examples;
